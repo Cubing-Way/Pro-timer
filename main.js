9562f0d7c16effae62e7d8292cbb3304dcad7040
Cubing-Way <legalmaneiro513@gmail.com>
2026-02-07 18:23:39 -0300

    update source

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ function restoreUI() {
 let scrDisplayFlag = false;
 
 
-document.getElementById("touchOverlay").addEventListener("touchstart", (e) => {
+document.getElementById("timer").addEventListener("touchstart", (e) => {
     e.preventDefault();
     if (e.repeat) return;
 
@@ -136,7 +136,7 @@ document.getElementById("touchOverlay").addEventListener("touchstart", (e) => {
     }
 });
 
-document.getElementById("touchOverlay").addEventListener("touchend", (e) => {
+document.getElementById("timer").addEventListener("touchend", (e) => {
     e.preventDefault();
     if (e.repeat) return;
 
